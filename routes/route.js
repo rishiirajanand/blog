3350866db713e2827623fcd31d65d55d34c529bd
rishiirajanand <rishirajanand5@gmail.com>
2024-06-19 15:16:57 +0530

    error fixing

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,6 @@
 import express from "express";
 import { signupUser, loginUser } from "../controller/user.controller.js";
 import { getImage, uploadImage } from "../controller/image.controller.js";
-import upload from '../utils/upload.js'
 
 import { createPost, getAllPosts, getPost, updatePost, deletePost } from "../controller/post.controller.js";
 import { authenticateToken } from "../controller/jwt.controller.js";
@@ -13,7 +12,7 @@ router.post('/signup', signupUser)
 router.post('/login', loginUser)
 
 
-router.post('/file/upload', upload.single('file'), uploadImage);
+
 router.get('/file/:filename' , getImage)
 
 router.post('/create', authenticateToken, createPost);
