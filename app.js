9f83f46cdd147463692bbf97c8dab447fcf33c75
rishiirajanand <rishirajanand5@gmail.com>
2024-06-19 14:58:51 +0530

    port change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 
 
 dotenv.config()
-const PORT = 8000;
+const PORT = process.env.PORT || 3100;
 const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
